5e43b29c197e612de6510da8a6c403b5262badf2
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-05-30 17:57:49 +0530

    Add IsoDataBank app to INSTALLED_APPS

--- archsadata/settings.py
+++ archsadata/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'isodatabank.apps.IsodatabankConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
