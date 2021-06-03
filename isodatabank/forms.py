526e3b9b3a4d458c6cb9869b0cd65eae6f0b4eb9
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-06-03 18:26:09 +0530

    Modify the DataForm to require name and email

--- isodatabank/forms.py
+++ isodatabank/forms.py
@@ -2,4 +2,6 @@ from django import forms
 
 
 class DataForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField(max_length=100)
     tilia_template_file = forms.FileField()
