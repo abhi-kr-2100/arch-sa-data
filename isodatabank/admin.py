8795f7feecc7db5d863b2806ae996c5ae03ab748
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-06-09 15:50:36 +0530

    Register ValidationInformation to admin site

--- isodatabank/admin.py
+++ isodatabank/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.urls import reverse
+
+from .models import ValidationInformation
+
+
+class ValidationAdmin(admin.ModelAdmin):
+    readonly_fields = ('submission_by', 'email', 'dataset_id')
+    fields = ('submission_by', 'email', 'dataset_id', 'validated')
+    
+    def view_on_site(self, obj):
+        url = reverse('download', args=[obj.dataset_id])
+        return url
+
+
+admin.site.register(ValidationInformation, ValidationAdmin)
